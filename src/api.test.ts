d61072f225545e5154420a5af44f15803048ba77
Brett Willemsen <bwillemsen@pagerduty.com>
2021-03-23 16:26:28 -0400

    Fix linting.

--- src/api.test.ts
+++ src/api.test.ts
@@ -305,12 +305,15 @@ test('API `delete` calls with shorthand `get` should succeed', async done => {
   done();
 });
 
-
 test('API catches ETIMEDOUT error', async done => {
-  nock('https://api.pagerduty.com').get('/incidents').replyWithError({code: 'ETIMEDOUT'})
+  nock('https://api.pagerduty.com')
+    .get('/incidents')
+    .replyWithError({code: 'ETIMEDOUT'});
 
   const pd = api({token: 'someToken1234567890'});
 
-  await expect(pd.get('/incidents')).rejects.toThrow('request to https://api.pagerduty.com/incidents failed, reason: undefined');
+  await expect(pd.get('/incidents')).rejects.toThrow(
+    'request to https://api.pagerduty.com/incidents failed, reason: undefined'
+  );
   done();
 });
