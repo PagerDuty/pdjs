eabec784f63d3b2721ea757af56f210d47af3c11
oleksii@xendit.co <oleksii@xendit.co>
2022-05-31 14:26:21 +0300

    chore: added RequestInit interface to a d.ts

--- build/src/common.d.ts
+++ build/src/common.d.ts
@@ -1,4 +1,19 @@
 declare type QueryParameter = Record<string, string | Array<string>>;
+interface RequestInit {
+    body?: BodyInit | null;
+    cache?: RequestCache;
+    credentials?: RequestCredentials;
+    headers?: HeadersInit;
+    integrity?: string;
+    keepalive?: boolean;
+    method?: string;
+    mode?: RequestMode;
+    redirect?: RequestRedirect;
+    referrer?: string;
+    referrerPolicy?: ReferrerPolicy;
+    signal?: AbortSignal | null;
+    window?: null;
+}
 export interface RequestOptions extends RequestInit {
     queryParameters?: QueryParameter;
     retryCount?: number;
